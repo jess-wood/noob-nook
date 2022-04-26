23ee83f230bf85980a7aebbfceee61d7c24736a4
jackson <jacksonfeinstein@gmail.com>
2022-04-26 12:16:55 -0700

    adding components

---
+++ src/Pages/GamesPages/GameComponents.js
@@ -0,0 +1,60 @@
+
+import Wordle from  "../GamesPages/Games/Wordle/Wordle";
+import Tetris from "../GamesPages/Games/Tetris/TetrisGame";
+import LightsOut from "../GamesPages/Games/Lights-Out/LightsOut";
+import SpaceGame from "../GamesPages/Games/SpaceGame/SpaceShooter";
+import Snake from "../GamesPages/Games/Snake/Snake";
+import Game_2048 from "../GamesPages/Games/_2048/src/components/mainBoard";
+import TypingMaster from "../GamesPages/Games/TypingMaster/TypingMaster";
+const gameComponents = (props) => {
+    return [
+
+        {
+            title: 'Tetris',
+            component: <Tetris/>,
+
+        },
+
+        {
+            title: 'Wordle',
+            component: <Wordle />
+        },
+        {
+            title: 'Lights Out',
+            component: <LightsOut />
+        },
+        {
+            title: 'Space Game',
+            component: <SpaceGame />
+        },
+        {
+            title: 'Snake',
+            component: <Snake />
+        },
+
+        {
+            title: '2048',
+            component: <Game_2048 />
+        },
+        {
+            title:'Typing Master',
+            component: <TypingMaster/>
+        },
+        // {
+        //     title:'Pong',
+        //     component: <Pong/>
+        // }
+    ];
+};
+
+
+// const containerComponents = (props) => {
+//     return [
+//         {
+//             title: 'Activities',
+//             component: <Wordle />
+//         }
+//     ];
+// };
+
+export {gameComponents};
