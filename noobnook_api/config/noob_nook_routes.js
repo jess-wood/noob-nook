45d44105510599fbc8c0debd35e4d125f42920ab
jacksonfeinstein <74090746+jacksonfeinstein@users.noreply.github.com>
2022-05-10 18:03:07 -0700

    Update noob_nook_routes.js

--- noobnook_api/config/noob_nook_routes.js
+++ noobnook_api/config/noob_nook_routes.js
@@ -87,6 +87,8 @@ highScoresRouter.get('/:username/get-wordleMinSec', HighScoresController.getHigh
 highScoresRouter.get('/:username/get-snake', HighScoresController.getHighScoreSnake);
 highScoresRouter.get('/:username/get-match', HighScoresController.getHighScoreMemory);
 highScoresRouter.get('/:username/get-connect4', HighScoresController.getHighScoreConnect4);
+highScoresRouter.get('/:username/get-2048', HighScoresController.getHighScore2048);
+
 highScoresRouter.get('/:score/:username/score-wordle', HighScoresController.postNewHighScoreWordle);
 highScoresRouter.get('/:score/:username/score-space', HighScoresController.postNewHighScoreSpace);
 highScoresRouter.get('/:score/:username/score-snake', HighScoresController.postNewHighScoreSnake);
@@ -97,6 +99,7 @@ highScoresRouter.get('/:score/:username/score-tetris', HighScoresController.post
 highScoresRouter.get('/:score/:username/score-wordleMin', HighScoresController.postNewHighScoreWordleMin);
 highScoresRouter.get('/:score/:username/score-wordleSec', HighScoresController.postNewHighScoreWordleSec);
 highScoresRouter.get('/:score/:username/score-connect4', HighScoresController.postNewHighScoreConnect4);
+highScoresRouter.get('/:score/:username/score-2048', HighScoresController.postNewHighScore2048);
 
 const DashboardController = new (require('../app/Controllers/DashboardController.js'))();
 const dashboardRouter = require('koa-router')({
